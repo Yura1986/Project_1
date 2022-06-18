48307324e8b923c18847655031a88b668020b0e0
Yura <yaksen86@mail.ru>
2022-06-18 23:31:59 +0300

    Добавил логику else

--- Project_1/money.js
+++ Project_1/money.js
@@ -14,10 +14,12 @@ for( let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", " "),
         b = prompt("Во сколько обойдется?");
 
-        if( (typeof(a) ) ==='string' && (typeof(a)) != null && (typeof(b)) != null &&
+        if ( (typeof(a) ) ==='string' && (typeof(a)) != null && (typeof(b)) != null &&
         a != "" && b != ""  && a.length < 50) {
             console.log("DONE")
             appData.expenses[a] = b; 
+        } else{
+            i = i - 1
         }
 
 }
